90b303f1721f7f08e6026b485b6ba3f84cadf0cf
lb <294894661@qq.com>
2024-11-11 11:09:19 +0800

    update

--- app/[locale]/(with-footer)/category/[code]/page.tsx
+++ app/[locale]/(with-footer)/category/[code]/page.tsx
@@ -30,7 +30,7 @@ export default async function Page({ params }: { params: { code: string } }) {
     supabase
       .from('web_navigation')
       .select('*', { count: 'exact' })
-      .eq('category_name', params.code)
+      .like('category_name', `%${params.code}%`)
       .range(0, InfoPageSize - 1),
   ]);
 
