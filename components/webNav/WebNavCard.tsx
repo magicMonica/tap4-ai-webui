8a9901b662b05a6bfcd5b5beed2b5bcb5bda4ff3
lb <294894661@qq.com>
2024-11-19 23:02:44 +0800

    UPDATE

--- components/webNav/WebNavCard.tsx
+++ components/webNav/WebNavCard.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { GsGameInfo } from '@/db/supabase/types';
-import { CircleArrowRight } from 'lucide-react';
+import { CircleArrowRight, SquareArrowOutUpRight } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 
 export default function WebNavCard({ name, thumbnail_url, title, content }: GsGameInfo) {
@@ -15,15 +15,24 @@ export default function WebNavCard({ name, thumbnail_url, title, content }: GsGa
           src={thumbnail_url || ''}
           alt={title || ''}
           title={title || ''}
-          width={288}
-          height={165}
-          className='aspect-video w-full rounded-xl bg-white/20 transition-opacity hover:opacity-80'
+          width={310}
+          height={174}
+          className='aspect-[310/174] w-full rounded-xl bg-white/40 hover:opacity-70'
         />
-        <div className='absolute inset-0 z-10 hidden items-center justify-center gap-1 rounded-xl bg-black/40 text-xl text-white transition-all duration-300 group-hover:flex'>
+        <div className='absolute inset-0 z-10 hidden items-center justify-center gap-1 rounded-xl bg-black bg-opacity-50 text-xl text-white transition-all duration-200 group-hover:flex'>
           {t('checkDetail')} <CircleArrowRight className='size-4' />
         </div>
       </Link>
-      <p className='line-clamp-2 px-1 text-xs text-white/70 lg:line-clamp-3'>{content || ''}</p>
+      <div className='flex items-center justify-between px-[6px]'>
+        <a href={thumbnail_url || ''} title={title || ''} target='_blank' rel='nofollow' className='hover:opacity-70'>
+          <h3 className='line-clamp-1 flex-1 text-sm font-bold lg:text-base'>{title || ''}</h3>
+        </a>
+        <a href={thumbnail_url || ''} title={title || ''} target='_blank' rel='nofollow' className='hover:opacity-70'>
+          <SquareArrowOutUpRight className='size-5' />
+          <span className='sr-only'>{title}</span>
+        </a>
+      </div>
+      <p className='line-clamp-3 px-[6px] text-xs text-white/70 lg:line-clamp-5 lg:text-sm'>{content}</p>
     </div>
   );
 }
